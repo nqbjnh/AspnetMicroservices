bab073d94362e539a8a92117476573906b8cefd1
Nguyễn Quang Bình <binhnguyen@usexpress.vn>
2021-10-17 15:12:58 +0700

    completed section 12

---
+++ src/WebApps/AspnetRunBasics/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRunBasics.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
